02f2bcd99169546cd81d84138e6bef5f08f04a41
Edint386 <jason62570127@163.com>
2024-09-14 03:52:07 +1000

    feat(init): initial commit with standardized types: MessageTypes, EventTypes, and ChannelTypes

---
+++ heychat/_types.py
@@ -0,0 +1,14 @@
+# _types.py
+
+class MessageTypes:
+    IMG = 3
+    MD = 4
+    MD_WITH_MENTION = 5
+
+class EventTypes:
+    JOINED_GUILD = 'JOINED_GUILD'
+    # 其他事件类型
+
+class ChannelTypes:
+    TEXT = 1
+    # 其他频道类型
