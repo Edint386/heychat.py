4548b1468df75d9bf9d3809d79b856662d2cc40b
Edint386 <jason62570127@163.com>
2024-09-14 18:38:02 +1000

    feat(types): add enum to class

--- heychat/_types.py
+++ heychat/_types.py
@@ -1,6 +1,8 @@
 # _types.py
+import enum
 
-class MessageTypes:
+@enum.unique
+class MessageTypes(enum.Enum):
     IMG = 3
     MD = 4
     MD_WITH_MENTION = 10
@@ -10,13 +12,11 @@ class EventTypes:
     # JOINED_GUILD = 'JOINED_GUILD'
     # 其他事件类型
 
-class ChannelTypes:
+@enum.unique
+class ChannelTypes(enum.Enum):
     VOICE = 0
     TEXT = 1
     ANNOUNCEMENT = 2
     CATEGORY = 3
     TEMPORARY = 4
     TEMPOR_GENERATOR = 5
-
-
-    # 其他频道类型
