66821cc0bf15f9ec9cc03c95ea6d8143a1e56c83
Edint386 <jason62570127@163.com>
2024-09-14 12:33:47 +1000

    fix(types): fix msgtypes and channeltypes

--- heychat/_types.py
+++ heychat/_types.py
@@ -3,12 +3,20 @@
 class MessageTypes:
     IMG = 3
     MD = 4
-    MD_WITH_MENTION = 5
+    MD_WITH_MENTION = 10
 
 class EventTypes:
-    JOINED_GUILD = 'JOINED_GUILD'
+    pass
+    # JOINED_GUILD = 'JOINED_GUILD'
     # 其他事件类型
 
 class ChannelTypes:
+    VOICE = 0
     TEXT = 1
+    ANNOUNCEMENT = 2
+    CATEGORY = 3
+    TEMPORARY = 4
+    TEMPOR_GENERATOR = 5
+
+
     # 其他频道类型
