8705273f70f4d65e8190f6ceeb99dfe8c9c45d6e
Edint386 <jason62570127@163.com>
2024-09-14 18:39:46 +1000

    fix(channel): fix send function

--- heychat/channel.py
+++ heychat/channel.py
@@ -1,12 +1,14 @@
-# channel.py
+from ._types import ChannelTypes, MessageTypes
+from . import api
 
 class Channel:
-    def __init__(self, data):
+    def __init__(self, data, gate):
         self.id = data.get('channel_id')
         self.name = data.get('channel_name')
-        self.type = data.get('channel_type', 1)
-        self.guild_id = data.get('room_id')
+        self.type = ChannelTypes(data.get('channel_type')) if 'channel_type' in data else None
+        self.guild_id = data.get('room_id')  # 用于 bot.client.send
+        self.gate = gate  # 添加 gateway 实例
 
-    async def send(self, content):
-        # 调用 Bot 的 client.send 方法
-        pass
+    async def send(self, content, msg_type=MessageTypes.MD_WITH_MENTION):
+        # 使用 Gateway 的 send 方法
+        return await self.gate.exec_req(api.Message.create(self.id, content, msg_type.value,self.guild_id))
