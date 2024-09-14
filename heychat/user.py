b49e6098caad70407f410e64a475ce07c85c8636
Edint386 <jason62570127@163.com>
2024-09-14 18:45:01 +1000

    fix(user): fix json path

--- heychat/user.py
+++ heychat/user.py
@@ -2,14 +2,15 @@
 
 class User:
     def __init__(self, data):
-        self.id = data.get('user_id')
-        self.username = data.get('nickname',None)
-        self.avatar = data.get('user_base_info', {}).get('avatar', None)
-        self.bot = data.get('bot', False)
-        self.nickname = data.get('nickname',None)
+        basic_info = data.get('user_base_info', {})
+        self.id = basic_info.get('user_id')
+        self.username = basic_info.get('nickname',None)
+        self.avatar = basic_info.get('avatar', None)
+        self.bot = basic_info.get('bot', False)
+        self.nickname = basic_info.get('room_nickname',None)
         if not self.nickname:
             self.nickname = self.username
-        self.roles = data.get('roles', [])
-        self.tags = data.get('tags', [])
-        self.level = data.get('level', 0)
-        self.online = data.get('online_state', 0)
+        self.roles = basic_info.get('roles', [])
+        self.tags = basic_info.get('tag', None)
+        self.level = basic_info.get('level', 0)
+        self.online = basic_info.get('online_state', 0)
