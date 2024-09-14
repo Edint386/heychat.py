688c5bbcf3cadd8ca1a90160eb3806534e42f6a4
Edint386 <jason62570127@163.com>
2024-09-14 12:38:15 +1000

    feat(user): improve user data

--- heychat/user.py
+++ heychat/user.py
@@ -3,9 +3,13 @@
 class User:
     def __init__(self, data):
         self.id = data.get('user_id')
-        self.username = data.get('nickname')
-        self.avatar = data.get('avatar')
+        self.username = data.get('nickname',None)
+        self.avatar = data.get('user_base_info', {}).get('avatar', None)
         self.bot = data.get('bot', False)
-        self.nickname = data.get('nickname')
+        self.nickname = data.get('nickname',None)
+        if not self.nickname:
+            self.nickname = self.username
         self.roles = data.get('roles', [])
+        self.tags = data.get('tags', [])
+        self.level = data.get('level', 0)
         self.online = data.get('online_state', 0)
