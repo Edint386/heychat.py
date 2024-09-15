a2e343f4087aa8ca21f041498eba61411c935b67
Edint386 <jason62570127@163.com>
2024-09-16 00:35:08 +1000

    fix(client): fix func upload file reader

--- heychat/client.py
+++ heychat/client.py
@@ -3,6 +3,7 @@ from typing import Union
 
 import aiohttp
 
+from . import api
 from ._types import MessageTypes
 from .channel import PublicTextChannel, PublicVoiceChannel
 from .gateway import Gateway
@@ -33,20 +34,15 @@ class Client:
         """
         :param file: file path or binary data
         """
-        data = aiohttp.FormData()
         if isinstance(file, str):
-            data.add_field('file', open(file, 'rb'))
+            with open(file, 'rb') as f:
+                res = await self.gate.exec_req(api.File.upload(f))
         else:
-            data.add_field('file', file)
-        await self.requestor('POST', 'upload', data=data)
+            res = await self.gate.exec_req(api.File.upload(file))
+        return res['result']['url']
 
     async def requestor(self, method, endpoint, **kwargs):
-        url = f'{self.base_url}/{endpoint}'
-        async with self.session.request(method, url, headers=self.headers, params=self.params, **kwargs) as resp:
-            responses = await resp.json()
-        if responses.get("status") == "failed":
-            raise Exception(responses.get('msg'))
-        return responses
+        return await self.gate.request(method, endpoint, **kwargs)
 
     async def start(self):
         await asyncio.gather(self.gate.run())
