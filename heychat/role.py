2c10b4f2536e05773e1f6270d010bc4be7ed7db6
Edint386 <jason62570127@163.com>
2024-09-15 17:33:33 +1000

    fix(role)

--- heychat/role.py
+++ heychat/role.py
@@ -1,6 +1,11 @@
 # role.py
 
 class Role:
-    def __init__(self, data):
-        self.id = data.get('role_id')
-        self.name = data.get('role_name')
+    def __init__(self, **kwargs):
+        self.role_id: str = kwargs.get("role_id", str())
+        self.name: str = kwargs.get("name", "")
+        # self.color: int = kwargs.get("color", 0)
+        # self.position: int = kwargs.get("position", 0)
+        # self.hoist: int = kwargs.get("hoist", 0)
+        # self.mentionable: int = kwargs.get("mentionable", 0)
+        # self.permissions: int = kwargs.get("permissions", 0)
