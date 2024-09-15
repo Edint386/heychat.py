59b3bbee1703d6699fb595b9390926784393b276
Edint386 <jason62570127@163.com>
2024-09-15 17:33:56 +1000

    fix(user): adapt user.py with type 50 message

--- heychat/user.py
+++ heychat/user.py
@@ -1,16 +1,16 @@
 # user.py
+from .role import Role
 
 class User:
     def __init__(self, data):
-        basic_info = data.get('user_base_info', {})
-        self.id = basic_info.get('user_id')
-        self.username = basic_info.get('nickname',None)
-        self.avatar = basic_info.get('avatar', None)
-        self.bot = basic_info.get('bot', False)
-        self.nickname = basic_info.get('room_nickname',None)
-        if not self.nickname:
-            self.nickname = self.username
-        self.roles = basic_info.get('roles', [])
-        self.tags = basic_info.get('tag', None)
-        self.level = basic_info.get('level', 0)
-        self.online = basic_info.get('online_state', 0)
+        self.id: int = data.get('user_id')
+        self.username: str = data.get('nickname',None)
+        self.nickname: str = data.get('room_nickname', None) or self.username
+        self.avatar: str = data.get('avatar', None)
+        self.bot: bool = data.get('bot', False)
+
+        self.roles: list = [Role(id=role) for role in data.get('roles', [])]
+
+        self.tags: list = data.get('tag', None)
+        self.level: int = data.get('level', 0)
+        self.online: int = data.get('online_state', 0)
