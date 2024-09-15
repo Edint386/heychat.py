1b70970c694c0dab288b1bcb9b7cbb10ccc75419
Edint386 <jason62570127@163.com>
2024-09-15 17:29:34 +1000

    feat(channel): modify channel with new channel type

--- heychat/context.py
+++ heychat/context.py
@@ -1,7 +1,7 @@
+from .channel import public_channel_factory, PublicChannel
 from .guild import Guild
-from .channel import Channel
 
 class Context:
     def __init__(self, data, gate):
-        self.guild = Guild(data)
-        self.channel = Channel(data, gate)
+        self.guild: Guild = Guild(data)
+        self.channel: PublicChannel = public_channel_factory(data, gate)
