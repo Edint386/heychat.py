c4ec009121570fe1810c7655abb1e3e20dfd1981
Edint386 <jason62570127@163.com>
2024-09-15 22:43:46 +1000

    fix(gateway): reformat code

--- heychat/gateway.py
+++ heychat/gateway.py
@@ -1,13 +1,14 @@
 from .requester import Requester
 from .api import _Req
 
+
 class Gateway:
-    def __init__(self, token,receiver):
+    def __init__(self, token, receiver):
         self.token = token
         self.requester = Requester(token)
         self.receiver = receiver
 
-    async def request(self, method, path,  **params):
+    async def request(self, method, path, **params):
         return await self.requester.request(method, path, **params)
 
     async def exec_req(self, r: _Req):
