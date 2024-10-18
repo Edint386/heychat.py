1cc8e8175bf5b3b2c89b0e91d407a80b4ed65322
Edint386 <jason62570127@163.com>
2024-10-19 06:28:02 +1100

    feat(channel): update message

--- heychat/channel.py
+++ heychat/channel.py
@@ -22,17 +22,34 @@ class PublicTextChannel(PublicChannel):
     def __init__(self, data, gate):
         super().__init__(data, gate)
 
-    async def send(self, content: Union[str, MDMessage], msg_type=MessageTypes.MD_WITH_MENTION, **kwargs):
+    async def send(self, content: Union[str, MDMessage], msg_type = MessageTypes.MD_WITH_MENTION, **kwargs):
         if isinstance(content, MDMessage):
             if content.count() == 1 and isinstance(content[0], Element.Image):
+                # 如果mdmessage只包含一个图片元素，直接发送图片消息
                 img_url = content[0].src
                 req = api.Message.create(self.id, MessageTypes.IMG, self.guild_id, img=img_url, **content.extra_info)
             else:
+                # 其他mdmessage或包含多个元素的mdmessage，发送富文本消息
                 req = api.Message.create(self.id, MessageTypes.MD_WITH_MENTION, self.guild_id, msg=str(content),
                                          **content.extra_info)
         else:
+            # 普通文本消息
             req = api.Message.create(self.id, msg_type, self.guild_id, msg=content, **kwargs)
         return await self.gate.exec_req(req)
+
+    async def update_message(self, msg_id:str, content:Union[str, MDMessage],
+                             msg_type = MessageTypes.MD_WITH_MENTION, reply_id=None, **kwargs):
+        data = {
+            'msg_id': msg_id,
+            'msg': content,
+            'room_id': self.guild_id,
+            'channel_id': self.id,
+            'msg_type': msg_type,
+            **kwargs}
+        if reply_id:
+            data['reply_id'] = reply_id
+
+        return await self.gate.exec_req(api.Message.update(**data))
 
 
 class PublicVoiceChannel(PublicChannel):
