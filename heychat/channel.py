d71f2476578a322828ccff8fe117fd2d38a48fbf
Edint386 <jason62570127@163.com>
2024-10-28 14:57:28 +1100

    fix(channel): send message to voice channel

--- heychat/channel.py
+++ heychat/channel.py
@@ -1,3 +1,4 @@
+# channel.py
 from typing import Union
 
 from ._types import ChannelTypes, MessageTypes
@@ -17,20 +18,9 @@ class PublicChannel:
         self.guild_id: str = data.get("room_base_info").get('room_id')  # 用于 bot.client.send
         self.gate: Gateway = gate  # 添加 gateway 实例
 
-
-class PublicTextChannel(PublicChannel):
-    def __init__(self, data, gate):
-        super().__init__(data, gate)
-
     async def send(self, content: Union[str, MDMessage], msg_type = MessageTypes.MD_WITH_MENTION, **kwargs):
         if isinstance(content, MDMessage):
-            if content.count() == 1 and isinstance(content[0], Element.Image):
-                # 如果mdmessage只包含一个图片元素，直接发送图片消息
-                img_url = content[0].src
-                req = api.Message.create(self.id, MessageTypes.IMG, self.guild_id, img=img_url, **content.extra_info)
-            else:
-                # 其他mdmessage或包含多个元素的mdmessage，发送富文本消息
-                req = api.Message.create(self.id, MessageTypes.MD_WITH_MENTION, self.guild_id, msg=str(content),
+            req = api.Message.create(self.id, MessageTypes.MD_WITH_MENTION, self.guild_id, msg=str(content),
                                          **content.extra_info)
         else:
             # 普通文本消息
@@ -41,11 +31,17 @@ class PublicTextChannel(PublicChannel):
                              msg_type = MessageTypes.MD_WITH_MENTION, reply_id=None, **kwargs):
         data = {
             'msg_id': msg_id,
-            'msg': content,
             'room_id': self.guild_id,
             'channel_id': self.id,
             'msg_type': msg_type,
             **kwargs}
+
+        if isinstance(content, MDMessage):
+            data = {**data, **content.extra_info}
+            content = str(content)
+
+        data['msg'] = content
+
         if reply_id:
             data['reply_id'] = reply_id
 
@@ -55,18 +51,25 @@ class PublicTextChannel(PublicChannel):
         return await self.gate.exec_req(api.Message.delete(msg_id, self.guild_id, self.id))
 
 
+class PublicTextChannel(PublicChannel):
+    def __init__(self, data, gate):
+        super().__init__(data, gate)
+
+
+
+
 class PublicVoiceChannel(PublicChannel):
     def __init__(self, data, gate):
         super().__init__(data, gate)
 
 
-class PrivateChannel(PublicTextChannel):
-    def __init__(self, data, gate):
-        super().__init__(data, gate)
-
-    async def send(self, content, msg_type=MessageTypes.MD_WITH_MENTION):
-        # 当前暂无该接口
-        raise NotImplementedError
+# class PrivateChannel(PublicChannel):
+#     def __init__(self, data, gate):
+#         super().__init__(data, gate)
+#
+#     async def send(self, content, msg_type=MessageTypes.MD_WITH_MENTION):
+#         # 当前暂无该接口
+#         raise NotImplementedError
 
 
 def public_channel_factory(data, gate) -> Union[PublicTextChannel, PublicVoiceChannel]:
