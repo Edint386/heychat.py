fa6e9be3326494e75e18834a43a3c4d992c23213
Edint386 <jason62570127@163.com>
2024-11-21 09:00:49 +1100

    fix(channel): remove unused import

--- heychat/channel.py
+++ heychat/channel.py
@@ -5,7 +5,7 @@ from typing import Union
 from ._types import ChannelTypes, MessageTypes
 from . import api
 from .gateway import Gateway
-from .mdmessage import MDMessage, Element
+from .mdmessage import MDMessage
 
 
 class PublicChannel:
