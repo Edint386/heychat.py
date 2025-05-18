235dcb8f5fd9f7d62a07287b3a19f0eba6906c8b
Edint386 <jason62570127@163.com>
2025-05-19 08:51:24 +1000

    feat(user): add private send api function

--- heychat/user.py
+++ heychat/user.py
@@ -1,10 +1,14 @@
 # user.py
-from typing import List
+from typing import List, Union
 from .role import Role
+from ._types import MessageTypes
+from . import api
+import json
+from .mdmessage import MDMessage
 
 
 class User:
-    def __init__(self, data):
+    def __init__(self, data, gate):
         self.id: int = int(data.get('user_id'))
         self.username: str = data.get('nickname', None)
         self.nickname: str = data.get('room_nickname', None) or self.username
@@ -14,3 +18,23 @@ class User:
         self.tags: list = data.get('tag', None)
         self.level: int = data.get('level', 0)
         self.online: int = data.get('online_state', 0)
+        self.gate = gate
+
+    async def send(self, content: Union[str, MDMessage, dict], msg_type=MessageTypes.MD_WITH_MENTION, **kwargs):
+        """Send a direct message to this user."""
+        if not self.gate:
+            raise ValueError("Gateway is required to send messages")
+
+        if isinstance(content, dict):
+            # 卡片消息
+            req = api.UserMessage.send(self.id, MessageTypes.CARD, json.dumps(content), **kwargs)
+        elif isinstance(content, MDMessage):
+            # MD消息
+            req = api.UserMessage.send(self.id, MessageTypes.MD_WITH_MENTION, str(content), 
+                                      addition=json.dumps(content.extra_info) if content.extra_info else "{}", 
+                                      **kwargs)
+        else:
+            # 普通文本消息
+            req = api.UserMessage.send(self.id, msg_type, str(content), **kwargs)
+            
+        return await self.gate.exec_req(req)
